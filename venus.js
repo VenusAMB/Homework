12ad028a3d73952e2d870ec51576848381801ef7
Tmorgan33 <ms.terry.l.morgan@gmail.com>
2020-01-31 16:40:42 -0500

    added homework to repo

--- venus.js
+++ venus.js
@@ -1,1 +1,56 @@
 var name = 2
+var name = 2
+var canning = "pickles";
+var jars = 5;
+// console.log(canning * jars);
+// Bonus js
+var str = "I am a teapot";
+var test = 2 + 2;
+console.log(test)
+var name = 2 + true;
+// Exercise conditional 
+//var i = 2; 
+console.log(i+5);
+console.log((i+5)*2);
+console.log(14%3); //2 modulo = % and answer is remainder of whats left
+//Yes returned as expected
+var i = 5 //logs variable was not even (!var i = 5)
+console.log(i+5);
+console.log((i+5)*2);
+console.log(18%3);
+if(i==0 || i<100){
+    console.log("math");
+} //test if variable modulus 2 returns 0
+var haveMoney=1
+var driveToStore=2
+var ICanBuyFruit=3
+var rainyOutside=4
+var wearRaincoat=5
+var ICanStayDry=6
+var theTVisOwn=7
+var sitToWatchTV=8
+var ThenICanBeEntertained=9
+var itIsColdOutside=10
+var WearACoat=11
+var thenICanStayWarm=12
+var foodIsClean=13
+var foodIsFresh=14
+var thenFoodIsGood=15
+if(haveMoney && driveToStore){ICanBuyFruit}
+if(rainyOutside && wearRaincoat){ICanStayDry}
+if(theTVisOwn && sitToWatchTV){ThenICanBeEntertained}
+if(itIsColdOutside && WearACoat){thenICanStayWarm}
+if(foodIsClean || foodIsFresh){thenFoodIsGood}
+//If statements = both need to exist and conditional statment = only one can exist
+
+// Exercise loop
+var i = 0;
+while(i!==50){
+    console.log(i);
+    i+=2;
+    i+=2;
+if(i%2===0){
+        console.log(i)
+    }
+    i++;
+}
