fa359b5ec05dc95ae5cb8bf468ed3b0d37a15fb6
Tmorgan33 <ms.terry.l.morgan@gmail.com>
2020-02-07 12:08:34 -0500

    Exercise array Grocery Store Items

--- venus.js
+++ venus.js
@@ -8,111 +8,116 @@ var str = "I am a teapot";
 var test = 2 + 2;
 console.log(test)
 var name = 2 + true;
-// Exercise conditional 
-//var i = 2; 
-console.log(i+5);
-console.log((i+5)*2);
-console.log(14%3); //2 modulo = % and answer is remainder of whats left
-//Yes returned as expected
-var i = 5 //logs variable was not even (!var i = 5)
-console.log(i+5);
-console.log((i+5)*2);
-console.log(18%3);
-if(i==0 || i<100){
-    console.log("math");
-} //test if variable modulus 2 returns 0
-var haveMoney=1
-var driveToStore=2
-var ICanBuyFruit=3
-var rainyOutside=4
-var wearRaincoat=5
-var ICanStayDry=6
-var theTVisOwn=7
-var sitToWatchTV=8
-var ThenICanBeEntertained=9
-var itIsColdOutside=10
-var WearACoat=11
-var thenICanStayWarm=12
-var foodIsClean=13
-var foodIsFresh=14
-var thenFoodIsGood=15
-if(haveMoney && driveToStore){ICanBuyFruit}
-if(rainyOutside && wearRaincoat){ICanStayDry}
-if(theTVisOwn && sitToWatchTV){ThenICanBeEntertained}
-if(itIsColdOutside && WearACoat){thenICanStayWarm}
-if(foodIsClean || foodIsFresh){thenFoodIsGood}
-//If statements = both need to exist and conditional statment = only one can exist
+// // Exercise conditional 
+// //var i = 2; 
+// console.log(i+5);
+// console.log((i+5)*2);
+// console.log(14%3); //2 modulo = % and answer is remainder of whats left
+// //Yes returned as expected
+// var i = 5 //logs variable was not even (!var i = 5)
+// console.log(i+5);
+// console.log((i+5)*2);
+// console.log(18%3);
+// if(i==0 || i<100){
+//     console.log("math");
+// } //test if variable modulus 2 returns 0
+// var haveMoney=1
+// var driveToStore=2
+// var ICanBuyFruit=3
+// var rainyOutside=4
+// var wearRaincoat=5
+// var ICanStayDry=6
+// var theTVisOwn=7
+// var sitToWatchTV=8
+// var ThenICanBeEntertained=9
+// var itIsColdOutside=10
+// var WearACoat=11
+// var thenICanStayWarm=12
+// var foodIsClean=13
+// var foodIsFresh=14
+// var thenFoodIsGood=15
+// if(haveMoney && driveToStore){ICanBuyFruit}
+// if(rainyOutside && wearRaincoat){ICanStayDry}
+// if(theTVisOwn && sitToWatchTV){ThenICanBeEntertained}
+// if(itIsColdOutside && WearACoat){thenICanStayWarm}
+// if(foodIsClean || foodIsFresh){thenFoodIsGood}
+// //If statements = both need to exist and conditional statment = only one can exist
 
-// Exercise loop
-var i = 0;
-while(i!==50){
-    console.log(i);
-    i+=2;
-    i+=2;
-if(i%2===0){
-        console.log(i)
-    }
-    i++;
-}
-//for loop
-for(vari=0; i <=50; i++){
-    console.log(i);
-    //while loop
-    var i = 1;
-    while (i<=50){
-        console.log (i)
-        i+=2;
-    }
-}
-//loop from 0 to 100
-var i=0;
-while (i<=100){
-    console.log(i);
-    i++;
-}
+// // Exercise loop
+// var i = 0;
+// while(i!==50){
+//     console.log(i);
+//     i+=2;
+//     i+=2;
+// if(i%2===0){
+//         console.log(i)
+//     }
+//     i++;
+// }
+// //for loop
+// for(vari=0; i <=50; i++){
+//     console.log(i);
+//     //while loop
+//     var i = 1;
+//     while (i<=50){
+//         console.log (i)
+//         i+=2;
+//     }
+// }
+// //loop from 0 to 100
+// var i=0;
+// while (i<=100){
+//     console.log(i);
+//     i++;
+// }
 
-// Exercise Function
+// // Exercise Function
 
-function MyFunction (functionX, functionY) {
-return functionX + functionY;
+// function MyFunction (functionX, functionY) {
+// return functionX + functionY;
+// } 
+// console.log("function output " + MyFunction(1,4));
+
+// // Exercise Function 2
+
+// function MyFunction2(A, B) {
+// return A * B;
+// } 
+// console.log("function output" * MyFunction2 (2,4)); 
+
+// // Exercise Function 3
+// function MyFunction3(A, B) {
+//     for (i = A; i<=B; i++){
+//         console.log(i+" Hello");
+//     }
+// }
+
+// MyFunction3 (5, 9);
+
+// // Exercise Function 4
+
+// var carColor ="green";
+// function myFunction4(Red, Blue) {
+// for (var i = 0; i<=Blue; i++){
+//     var carColor = Red;
+//        console.log (i + carColor);
+// }
+// }
+// myFunction4("red",4);
+// console.log(carColor);
+
+// // Exercise Math 
+// function randomCheck(){
+//    var userInput = document.getElementById('userInput').value;
+//    var random = Math.round(math.random()*25)
+//    random == userInput
+//    counsole.log ("success");
+
+// }
+
+//Exercise array Grocery Store Items
+var grogeryItems = ["grapes", "blueberries", "pineapple", "pickles", "chickpeas", "bananas", "tomatoes", "cookies", "cake", "pie"];
+//var array_groceryItems = ["grapes", "blueberries", "pineapple", "pickles", "chickpeas", "bananas", "tomatoes", "cookies", "cake", "pie"];
+for (var i=0; i < grogeryItems.length; i++){
+    console.log(grogeryItems[i]);
 } 
-console.log("function output " + MyFunction(1,4));
-
-// Exercise Function 2
-
-function MyFunction2(A, B) {
-return A * B;
-} 
-console.log("function output" * MyFunction2 (2,4)); 
-
-// Exercise Function 3
-function MyFunction3(A, B) {
-    for (i = A; i<=B; i++){
-        console.log(i+" Hello");
-    }
-}
-
-MyFunction3 (5, 9);
-
-// Exercise Function 4
-
-var carColor ="green";
-function myFunction4(Red, Blue) {
-for (var i = 0; i<=Blue; i++){
-    var carColor = Red;
-       console.log (i + carColor);
-}
-}
-myFunction4("red",4);
-console.log(carColor);
-
-// Excercise Math 
-function randomCheck(){
-   var userInput = document.getElementById('userInput').value;
-   var random = Math.round(math.random()*25)
-   random == userInput
-   counsole.log ("success");
-
-}
-
-
