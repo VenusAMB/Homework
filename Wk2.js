4b852c52df99027245b8a47130944f6ba76b788c
Tmorgan33 <ms.terry.l.morgan@gmail.com>
2020-02-11 15:28:11 -0500

    Exercise Dom Manipulation Homework wk2 js

---
+++ Wk2.js
@@ -0,0 +1,11 @@
+//Exercise Dom Manipulation
+//change styles of 3 separate elements using Javascript
+//change the class name and text of a child or sibling of each of the above elements
+
+document.getElementById("p1").style.color = "blue";
+document.getElementById("p2").style.color = "pink";
+document.getElementById("p3").style.color = "green";
+
+document.getElementById("p1").innerHTML = "New heading";
+document.getElementById("p2").innerHTML = "New subtitle heading";
+document.getElementById("p3").innerHTML = "New text";
