8a0f28fcfe02eab9f148ab7f02d0507722b1e0d6
VenusAMB <VenusSparkling4u@gmail.com>
2020-02-12 15:43:36 -0500

    settings

--- Wk2.js
+++ Wk2.js
@@ -3,7 +3,7 @@
 //change the class name and text of a child or sibling of each of the above elements
 
 document.getElementById("p1").style.color = "blue";
-document.getElementById("p2").style.color = "pink";
+document.getElementById("p2").style.color = "black";
 document.getElementById("p3").style.color = "green";
 
 document.getElementById("p1").innerHTML = "New heading";
