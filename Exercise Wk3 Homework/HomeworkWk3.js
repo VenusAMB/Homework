edc066d93bee82fb7e27b89e691ff641f35d6af9
VenusAMB <VenusSparkling4u@gmail.com>
2020-02-17 16:18:47 -0500

    not finished webpage

---
+++ Exercise Wk3 Homework/HomeworkWk3.js
@@ -0,0 +1,7 @@
+
+            function myFunc() {
+                // var c = document.body.children;
+                var txt = "Call for assistance 1-800-222-1234";
+                document.getElementById("demo").innerHTML = txt;
+            }
+            
